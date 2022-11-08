78aa5305846f0f77fe3e9855f056af36f94d0c98
mdferdousalam <mdferdousalam1989@yahoo.com>
2022-11-08 21:32:39 +0600

    header optimized

--- src/pages/shared/Header/Header.js
+++ src/pages/shared/Header/Header.js
@@ -21,29 +21,31 @@ const Header = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><Link to='/home'>Home</Link></li>
-                        <li><Link to='/blog'>Blog</Link></li>
-                        <li><Link to='/services'>Services</Link></li>
+                        <li><Link className='hover:bg-indigo-600 hover:text-white' to='/home'>Home</Link></li>
+                        <li><Link className='hover:bg-indigo-600 hover:text-white' to='/blog'>Blog</Link></li>
+                        <li><Link className='hover:bg-indigo-600 hover:text-white' to='/services'>Services</Link></li>
+                        <li><Link className='hover:bg-indigo-600 hover:text-white' to='/addservice'  >Add Service</Link></li>
+                        <li><Link className='hover:bg-indigo-600 hover:text-white' to='/myreviews'  >My Reviews</Link></li>
                     </ul>
                 </div>
             </div>
             <div className="flex navbar-center md:justify-between">
-                <Link to='/' className="btn btn-ghost normal-case text-xl">Electronic Security System Installer</Link>
+                <Link to='/' className="btn btn-ghost normal-case text-xl hover:bg-indigo-600 hover:text-white">Electronic Security System Installer</Link>
                 <div className='hidden md:inline'>
                     {
                         user?.uid ?
                             <>
-                                <Link to='/home' className="btn btn-ghost normal-case text-xl ">Home</Link>
-                                <Link to='/blog' className="btn btn-ghost normal-case text-xl">Blog</Link>
-                                <Link to='/services' className="btn btn-ghost normal-case text-xl">Services</Link>
-                                <Link to='/addservice' className="btn btn-ghost normal-case text-xl">Add Service</Link>
-                                <Link to='/myreviews' className="btn btn-ghost normal-case text-xl">My Reviews</Link>
+                                <Link to='/home' className="btn btn-ghost normal-case text-xl hover:bg-indigo-600 hover:text-white ">Home</Link>
+                                <Link to='/blog' className="btn btn-ghost normal-case text-xl hover:bg-indigo-600 hover:text-white">Blog</Link>
+                                <Link to='/services' className="btn btn-ghost normal-case text-xl hover:bg-indigo-600 hover:text-white">Services</Link>
+                                <Link to='/addservice' className="btn btn-ghost normal-case text-xl hover:bg-indigo-600 hover:text-white">Add Service</Link>
+                                <Link to='/myreviews' className="btn btn-ghost normal-case text-xl hover:bg-indigo-600 hover:text-white">My Reviews</Link>
                             </>
                             :
                             <>
-                                <Link to='/home' className="btn btn-ghost normal-case text-xl ">Home</Link>
-                                <Link to='/blog' className="btn btn-ghost normal-case text-xl">Blog</Link>
-                                <Link to='/services' className="btn btn-ghost normal-case text-xl">Services</Link>
+                                <Link to='/home' className="btn btn-ghost normal-case text-xl hover:bg-indigo-600 hover:text-white">Home</Link>
+                                <Link to='/blog' className="btn btn-ghost normal-case text-xl hover:bg-indigo-600 hover:text-white">Blog</Link>
+                                <Link to='/services' className="btn btn-ghost normal-case text-xl hover:bg-indigo-600 hover:text-white">Services</Link>
                             </>
                     }
 
@@ -55,21 +57,17 @@ const Header = () => {
                 {
                     user?.uid ?
                         <>
-                            <button onClick={handleLogOut} className="btn btn-ghost font-bold">
+                            <button onClick={handleLogOut} className="btn btn-ghost font-bold hover:bg-indigo-600 hover:text-white">
                                 <Link to='/logout'>Logout</Link>
                             </button>
-                            <button className="btn btn-ghost btn-circle">
-                                <div className="indicator">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
-                                </div>
-                            </button>
+
                         </>
                         :
                         <>
-                            <button className="btn btn-ghost font-bold">
+                            <button className="btn btn-ghost font-bold hover:bg-indigo-600 hover:text-white">
                                 <Link to='/login'>Login</Link>
                             </button>
-                            <button className="btn btn-ghost font-bold">
+                            <button className="btn btn-ghost font-bold hover:bg-indigo-600 hover:text-white">
                                 <Link to='/register'>Register</Link>
                             </button>
                         </>
